e9f743726b4a910b5c7551d644855c3645f41147
Rakibul Hasan <rakibulh170@gmail.com>
2020-06-27 19:48:33 +0600

    Update Day1 code

--- day_1_bank_note.py
+++ day_1_bank_note.py
@@ -1,4 +1,3 @@
-
 # Uri Online Judge (1018)
 
 
@@ -6,9 +5,11 @@ notes = [100, 50, 20, 10, 5, 2, 1]
 if __name__ == "__main__":
 
     note = int(input())
+    print(note)
     for i in range(7):
         if(note >= notes[i]):
-            print(note // notes[i], " note(s) of R$ ", notes[i])
+            print('{0} nota(s) de R$ {1},00'.format(note // notes[i],notes[i]))
+            #print(note // notes[i], "nota(s) de R$", notes[i]+",00")
             note %= notes[i]
         else:
-            print("0 note(s) of R$ ", notes[i])
+           print('0 nota(s) de R$ {0},00'.format(notes[i]))
